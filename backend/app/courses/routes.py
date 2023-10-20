869112282d17cc97350168a29e419b6461b4ac0a
Cate Lewison <clewison@hmc.edu>
2023-10-20 15:09:07 -0700

    Set up endpoints and search logic
    Parses JSON object and uses manually entered query

--- backend/app/courses/routes.py
+++ backend/app/courses/routes.py
@@ -1,10 +1,44 @@
 from flask import request, jsonify
 from . import courses
+import json
+
+# Do we need to check if data has course number, name, description, and timings?
+@courses.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @courses.route('/search', methods=['GET'])
 def search_courses():
-    query = request.args.get('q', '')
+  """Searches for courses that match the query string.
 
+  Args:
+    query: The query string to search for.
+    courses: A list of courses.
+
+  Returns:
+    A list of courses that match the query string.
+  """
+  # Change for specific file path
+  DATA = "C:/Users/catel/OneDrive/Documents/GitHub/p-classwise/backend/app/courses/data.json"
+
+  query = request.args.get('q', '')
+
+  with open(DATA, 'r') as file:
+     data = json.load(file)
+
+  results = []
+  for course in data:
+    if query.lower() in course['courseNumber'].lower() or query.lower() in course['courseTitle'].lower() or query.lower() in course['departmentExternalId'].lower() or query.lower() in course['institutionExternalId'].lower():
+       results.append(course)
+    """
+      "courseNumber": "CSCI136",
+      "courseTitle": "Computer Architecture",
+      "departmentExternalId": "CompSci",
+      "institutionExternalId": "PO",
+    """
     # TODO: Implement search logic to fetch courses based on the query from data.json
 
-    return jsonify([])  # Return empty list for now
+  return jsonify(results)
+
+
+#Testing: http://localhost:5000/courses/search?q=coursename
