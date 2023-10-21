26b9f1636515f3218e7833fd82ec6ccf26840377
ytaa2021 <ytaa2021@mymail.pomona.edu>
2023-10-20 17:44:39 -0700

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,7 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import React from 'react';
 
 import './App.css';
 import axios from 'axios';
-import BottomMenu from './components/BottomMenu';
-import SideMenu from './components/SideMenu';
-import Scheduler from './components/Calendar';
-
 
 const BASE_URL = 'TODO: BACKEND URL'; 
 
@@ -46,11 +20,8 @@ export const searchCourses = async (searchTerm) => {
 function App() {
   return (
     <div className="App">
-      <SideMenu />
       <h1>My Calendar</h1>
       <Scheduler />
-      <BottomMenu />
->>>>>>> Stashed changes
     </div>
   );
 }
