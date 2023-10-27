b8f4c354e89dd6f887dc05d4c5168c347e449ddc
Cate Lewison <catelewi@gmail.com>
2023-10-27 13:42:55 -0700

    Added schedule generation

--- backend/app/courses/routes.py
+++ backend/app/courses/routes.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from . import courses
 import json
+import random
 
 # Do we need to check if data has course number, name, description, and timings?
 @courses.route('/')
@@ -19,7 +20,7 @@ def search_courses():
     A list of courses that match the query string.
   """
   # Change for specific file path
-  DATA = "C:/Users/catel/OneDrive/Documents/GitHub/p-classwise/backend/app/courses/data.json"
+  DATA = "/Users/catelewison/Documents/GitHub/p-classwise/backend/app/courses/data.json"
 
   query = request.args.get('q', '')
 
@@ -29,7 +30,7 @@ def search_courses():
   results = []
   for course in data:
     if query.lower() in course['courseNumber'].lower() or query.lower() in course['courseTitle'].lower() or query.lower() in course['departmentExternalId'].lower() or query.lower() in course['institutionExternalId'].lower():
-       results.append(course)
+       results.append(course['courseNumber'])
     """
       "courseNumber": "CSCI136",
       "courseTitle": "Computer Architecture",
@@ -40,5 +41,41 @@ def search_courses():
 
   return jsonify(results)
 
-
 #Testing: http://localhost:5000/courses/search?q=coursename
+
+
+@courses.route('/generate', methods=['GET'])
+def generate_courses():
+  """Searches for courses that match the query string.
+
+  Args:
+    num: the number of courses to include in the schedule
+    courses: A list of courses.
+
+  Returns:
+    A schedule with specified number of courses.
+  """
+  num = request.args.get('n', '')
+  num = 2
+  # Change for specific file path
+  DATA = "/Users/catelewison/Documents/GitHub/p-classwise/backend/app/courses/data.json"
+
+  with open(DATA, 'r') as file:
+     data = json.load(file)
+
+  courses=[]
+  for course in data:
+    courses.append(course['courseNumber'])
+    """
+      "courseNumber": "CSCI136",
+      "courseTitle": "Computer Architecture",
+      "departmentExternalId": "CompSci",
+      "institutionExternalId": "PO",
+    """
+    # Catch edge case
+    if num < len(courses):
+       schedule = random.sample(courses, num)
+    # TODO: Implement search logic to fetch courses based on the query from data.json
+
+  print(schedule)
+  return schedule
